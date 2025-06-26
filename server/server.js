7d84b0398a9e4cc6c5ddf1a0a497dab152a6b3ac
kuldeep8630 <kuldeeppal8630@gmail.com>
2025-06-27 00:20:21 +0530

    new url for frontend

--- server/server.js
+++ server/server.js
@@ -8,9 +8,9 @@ require('dotenv').config();
 
 const app = express();
 
-console.log('Configuring CORS with origin:', 'https://stayfinder-frontend-2zmjof4lv-kuldeep-pals-projects-21041dff.vercel.app');
+console.log('Configuring CORS with origin:', 'https://stayfinder-frontend-7ygkr5zk7-kuldeep-pals-projects-21041dff.vercel.app');
 app.use(cors({
-  origin: 'https://stayfinder-frontend-2zmjof4lv-kuldeep-pals-projects-21041dff.vercel.app',
+  origin: 'https://stayfinder-frontend-7ygkr5zk7-kuldeep-pals-projects-21041dff.vercel.app',
 }));
 app.use(express.json());
 app.use('/uploads', express.static('uploads'));
